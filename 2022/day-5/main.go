ca007c8a3458c7bf6839091db9b8459eafcec400
Milan May'r <mayrmilan@gmail.com>
2022-12-10 14:04:12 -0600

    completed day 5 part two

--- 2022/day-5/main.go
+++ 2022/day-5/main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	print("The crates on top of each stack are: ")
 	println(topCrates())
+	print("CrateMover 9001 -- The crates on top of each stack are: ")
+	println(topCratesPartTwo())
 }
 
 func topCrates() (topCrates string) {
@@ -19,18 +21,7 @@ func topCrates() (topCrates string) {
 
 	// Create set of arrays representing stacks
 	// Stack 0 will be empty to make index association easy
-	stacks := make([][]string, 10)
-
-	for line := 7; line >= 0; line-- {
-		stack := 1
-		for charPosition := 1; charPosition < 35; charPosition += 4 {
-			crate := strings.Split(input[line], "")[charPosition]
-			if (crate != " ") {
-				stacks[stack] = append(stacks[stack], crate)
-			}
-			stack++
-		}
-	}
+	stacks := getStacks()
 
 	// Crate moving logic
 	for line := 10; line <= len(input)-1; line++ {
@@ -53,4 +44,52 @@ func topCrates() (topCrates string) {
 	}
 
 	return topCrates
+}
+
+func topCratesPartTwo() (topCrates string) {
+	input := utils.GetInput("input")
+	stacks := getStacks()
+
+	// Crate moving logic
+	for line := 10; line <= len(input)-1; line++ {
+		numberOfCrates, _ := strconv.Atoi(strings.Split(input[line], " ")[1])
+		source, _ := strconv.Atoi(strings.Split(input[line], " ")[3])
+		destination, _ := strconv.Atoi(strings.Split(input[line], " ")[5])
+
+		// get top n crates
+		cratesToMove := stacks[source][len(stacks[source])-numberOfCrates:len(stacks[source])]
+		// remove top n crates from source
+		stacks[source] = stacks[source][:len(stacks[source])-numberOfCrates]
+		// add top n crates to destination in order
+		stacks[destination] = append(stacks[destination], cratesToMove...)
+	}
+
+	for stack := 1; stack < len(stacks); stack++ {
+		topCrates = topCrates + stacks[stack][len(stacks[stack])-1]
+	}
+
+	return topCrates
+}
+
+func getStacks() (stacks [][]string) {
+	// get first 9 lines and split into 9 queues
+	// create the moving algorithm
+	input := utils.GetInput("input")
+
+	// Create set of arrays representing stacks
+	// Stack 0 will be empty to make index association easy
+	stacks = make([][]string, 10)
+
+	for line := 7; line >= 0; line-- {
+		stack := 1
+		for charPosition := 1; charPosition < 35; charPosition += 4 {
+			crate := strings.Split(input[line], "")[charPosition]
+			if (crate != " ") {
+				stacks[stack] = append(stacks[stack], crate)
+			}
+			stack++
+		}
+	}
+
+	return stacks
 }
