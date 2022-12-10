ce642e454ee3ade7001350bccd4b1eb813b33e49
Milan May'r <mayrmilan@gmail.com>
2022-12-10 14:36:06 -0600

    completed day 6 part 1

--- 2022/day-6/main.go
+++ 2022/day-6/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"strings"
 
+	"github.com/milanmayr/advent-of-code/2022/utils"
 )
 
 func main() {
+	print("The first start-of-packet marker appears at position: ")
+	println(firstStartOfPacketMarkerPosition())
+}
+
+func firstStartOfPacketMarkerPosition() (position int) {
+	input := utils.GetInput("input")
+	// input only has one line
+	datastream := strings.Split(input[0], "")
+
+	// start of a packet is indicated by four characters that are different
+	// get position of character that ends the first start-of-packet marker
+
+	potentialMarker := datastream[0:4]
 	
+	for char := 4; char < len(datastream)-1; char++ {
+		if !isMarkerUnique(potentialMarker) {
+			potentialMarker = potentialMarker[1:]
+			potentialMarker = append(potentialMarker, datastream[char])
+		} else {
+			position = char
+			break
+		}
+	}
+
+	return position
+}
+
+func isMarkerUnique(marker []string) (bool) {
+	for char := 0; char < len(marker)-1; char++ {
+		for otherchar := char+1; otherchar < len(marker); otherchar++ {
+			if marker[char] == marker[otherchar] {
+				return false
+			}
+		}
+	}
+	return true
 }
