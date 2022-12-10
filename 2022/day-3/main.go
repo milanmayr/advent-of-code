89a879da34f63698596d0109a3495c2fe0b62196
Milan May'r <mayrmilan@gmail.com>
2022-12-10 12:23:12 -0600

    fnished day 2 part two

--- 2022/day-3/main.go
+++ 2022/day-3/main.go
@@ -8,14 +8,17 @@ import (
 
 func main() {
 	print("The sum of priorities for all the shared items is: ")
-	print(sumPriorities())
+	println(sumPriorities())
+	print("The sum of priorities for the shared badges of each three-elf group is: ")
+	println(sumPrioritiesPartTwo())
 }
+
+var PriorityAlphabet string = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func sumPriorities() (sumOfPriorities int) {
 	// var sumOfPriorities int
 	input := utils.GetInput("input")
 
-	priorityAlphabet := "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	// split each line into two arrays. Length of each is half of the whole line
 	// compare both arrays to find the common character
 	// add priority of shared item to sumOfPriorities
@@ -41,7 +44,7 @@ func sumPriorities() (sumOfPriorities int) {
 				}
 				if string(firstChars[i]) == string(secondChars[j]) {
 					commonChar := string(firstChars[i])
-					priorityValue, err := utils.IndexOf(commonChar, strings.Split(priorityAlphabet, ""))
+					priorityValue, err := utils.IndexOf(commonChar, strings.Split(PriorityAlphabet, ""))
 					if err != nil {
 						panic("element was not found in string array")
 					}
@@ -52,6 +55,52 @@ func sumPriorities() (sumOfPriorities int) {
 			}
 		}
 	}
-
 	return sumOfPriorities
+}
+
+func sumPrioritiesPartTwo() (sumPriorities int) {
+	input := utils.GetInput("input")
+
+	sumPriorities = 0
+	for rucksack := 0; rucksack < len(input); rucksack += 3 {
+		firstRucksack := input[rucksack]
+		secondRucksack := input[rucksack+1]
+		thirdRucksack := input[rucksack+2]
+
+		firstChars := []rune(firstRucksack)
+		secondChars := []rune(secondRucksack)
+		thirdChars := []rune(thirdRucksack)
+
+		var commonCharFound bool
+		for i := 0; i < len(firstChars); i++ {
+			if commonCharFound {
+				break
+			}
+			for j := 0; j < len(secondChars); j++ {
+				if commonCharFound {
+					break
+				}
+				if string(firstChars[i]) == string(secondChars[j]) {
+					for k := 0; k < len(thirdChars); k++ {
+						if commonCharFound {
+							break
+						}
+						if string(secondChars[j]) == string(thirdChars[k]) {
+							commonChar := string(firstChars[i])
+							priorityValue, err := utils.IndexOf(commonChar, strings.Split(PriorityAlphabet, ""))
+							if err != nil {
+								panic("element was not found in string array")
+							}
+							sumPriorities += priorityValue
+							commonCharFound = true
+							break
+						}
+					}
+				}
+			}
+			
+		}
+	
+	}
+	return sumPriorities
 }
