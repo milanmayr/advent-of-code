e6f1f8032ac53ba63aadcc934ea89404ab164d9d
Milan May'r <mayrmilan@gmail.com>
2022-12-11 13:27:56 -0600

    completed day 11 part two

--- 2022/day-11/main.go
+++ 2022/day-11/main.go
@@ -10,41 +10,51 @@ import (
 
 func main() {
 	print("After 20 rounds of stuff-slinging simian shenanigans, the level of monkey business is: ")
-	println(getMonkeyBusinessLevel())
+	println(getMonkeyBusinessLevel(20, true))
+	print("After 10,000 rounds of stuff-slinging simian shenanigans and no longer dividing the worry level by 3, the level of monkey business is: ")
+	println(getMonkeyBusinessLevel(10000, false))
 }
 
 type monkey struct {
-	StartingItems []int
+	StartingItems []int64
 	Operation     struct {
 		Operator string
-		Number   int
+		Number   int64
 	}
-	DivisibleBy         int
+	DivisibleBy         int64
 	TargetMonkeyIfTrue  int
 	TargetMonkeyIfFalse int
-	Inspections         int
+	Inspections         int64
 	NumberIsOld         bool
 }
 
-func getMonkeyBusinessLevel() (level int) {
-	monkeys := playMonkeyBusiness(20)
-	var inspections []int
+func getMonkeyBusinessLevel(rounds int, partOne bool) (level int64) {
+	monkeys := playMonkeyBusiness(rounds, partOne)
+	var inspections []int64
 	for _, monkey := range monkeys {
 		inspections = append(inspections, monkey.Inspections)
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(inspections)))
+	sort.Slice(inspections, func(i, j int) bool { return inspections[i] > inspections[j] })
 	level = inspections[0] * inspections[1]
 	return level
 }
 
-func playMonkeyBusiness(rounds int) []*monkey {
+func playMonkeyBusiness(rounds int, divideByThree bool) []*monkey {
 	monkeys := getMonkeys()
+	var globalModulus int64
+	globalModulus = 1
 
+	for _, monkey := range monkeys {
+		globalModulus *= monkey.DivisibleBy
+	}
+	
 	for round := 1; round <= rounds; round++ {
+
+
 		for _, monkey := range monkeys {
 			// -- Start with first item
 			// 1. Operation -- increase worry level
-			// 2. Divide worry level by 3
+			// 2. Divide worry level by 3 (only if part one)
 			// 3. Test current worry level divisibility
 			// 4. Throw to monkey based on divisibility in step 3
 			//   a. Remove from beginning of slice for current monkey
@@ -65,15 +75,15 @@ func playMonkeyBusiness(rounds int) []*monkey {
 				switch operator {
 				case "*":
 					item *= monkey.Operation.Number
-				case "/":
-					item /= monkey.Operation.Number
 				case "+":
 					item += monkey.Operation.Number
-				case "-":
-					item -= monkey.Operation.Number
 				}
 
-				item /= 3
+				if divideByThree {
+					item /= 3
+				} else {
+					item = item % globalModulus
+				}
 
 				var targetMonkey int
 				if item%monkey.DivisibleBy == 0 {
@@ -101,14 +111,14 @@ func getMonkeys() []*monkey {
 
 	for line := 0; line < len(input)-1; line += 7 {
 		startingItemsStrings := strings.Split(input[line+1][18:], ", ")
-		var startingItems []int
+		var startingItems []int64
 		for _, item := range startingItemsStrings {
-			itemInt, _ := strconv.Atoi(item)
+			itemInt, _ := strconv.ParseInt(item, 10, 64)
 			startingItems = append(startingItems, itemInt)
 		}
 
 		operator := input[line+2][23:24]
-		number, err := strconv.Atoi(input[line+2][25:])
+		number, err := strconv.ParseInt(input[line+2][25:], 10, 64)
 		var numberIsOld bool
 		if err != nil {
 			if input[line+2][25:] == "old" {
@@ -116,7 +126,7 @@ func getMonkeys() []*monkey {
 			}
 		}
 
-		divisibleBy, _ := strconv.Atoi(input[line+3][21:])
+		divisibleBy, _ := strconv.ParseInt(input[line+3][21:], 10, 64)
 
 		targetMonkeyIfTrue, _ := strconv.Atoi(strings.Split(input[line+4], "throw to monkey ")[1])
 		targetMonkeyIfFalse, _ := strconv.Atoi(strings.Split(input[line+5], "throw to monkey ")[1])
@@ -125,7 +135,7 @@ func getMonkeys() []*monkey {
 			StartingItems: startingItems,
 			Operation: struct {
 				Operator string
-				Number   int
+				Number   int64
 			}{
 				Operator: operator,
 				Number:   number,
