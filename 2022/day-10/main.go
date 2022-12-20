40de2c8efa2063a774b203ebc46cdfe34a2c5b23
Milan May'r <mayrmilan@gmail.com>
2022-12-20 14:32:47 -0600

    day 10 -- INCOMPLETE

--- 2022/day-10/main.go
+++ 2022/day-10/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/milanmayr/advent-of-code/2022/utils"
 )
 
 func main() {
-	input := utils.GetInput("input")
+	print("The sum of the six signal strengths is: ")
+	println(sumOfSixSignalStrengths())
+}
 
-	X := 1
-	for cycle := 1; cycle <= 220; cycle++ {
-	}
+func sumOfSixSignalStrengths() (X int) {
+	input := utils.GetInput("example2-input")
+
+	X = 1
+	cycle := 1
+
+	numberToAdd := 0
 	for _, line := range input {
-		cycle++
-
+		if line == "noop" {
+			cycle++
+		}
+		if line != "noop" {
+			numberToAdd, _ = strconv.Atoi(line[5:])
+			X += numberToAdd
+			cycle ++
+			cycle ++
+		}
 	}
+
+	return X
 }
