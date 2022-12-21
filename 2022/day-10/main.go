ac69aa36d766574d791251d7e30d9f783e5d0342
Milan May'r <mayrmilan@gmail.com>
2022-12-20 21:57:22 -0600

    day 10 part one complete!

--- 2022/day-10/main.go
+++ 2022/day-10/main.go
@@ -1,34 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/milanmayr/advent-of-code/2022/utils"
 )
 
 func main() {
-	print("The sum of the six signal strengths is: ")
-	println(sumOfSixSignalStrengths())
+	sum := sumOfSixSignalStrengths()
+	print("The sum of all six signal strengths is ")
+	println(sum)
 }
 
-func sumOfSixSignalStrengths() (X int) {
-	input := utils.GetInput("example2-input")
+func sumOfSixSignalStrengths() (sum int) {
+	input := utils.GetInput("input")
 
-	X = 1
-	cycle := 1
-
-	numberToAdd := 0
-	for _, line := range input {
-		if line == "noop" {
-			cycle++
-		}
-		if line != "noop" {
-			numberToAdd, _ = strconv.Atoi(line[5:])
-			X += numberToAdd
-			cycle ++
-			cycle ++
-		}
+	cycle := 0
+	X := 1
+	thCycles := map[int]bool{
+		20:  true,
+		60:  true,
+		100: true,
+		140: true,
+		180: true,
+		220: true,
 	}
 
-	return X
+	for _, line := range input {
+		if line == "noop" {
+			cycleTick(&cycle, thCycles, &X, &sum)
+		} else if line != "noop" {
+			numberToAdd, _ := strconv.Atoi(line[5:])
+			cycleTick(&cycle, thCycles, &X, &sum)
+			cycleTick(&cycle, thCycles, &X, &sum)
+			X += numberToAdd
+		}
+	}
+	return sum
+}
+
+func cycleTick(cycle *int, thCycles map[int]bool, X *int, sum *int) {
+	msg := "The signal strength during the %dth cycle is %d"
+
+	*cycle++
+
+	if thCycles[*cycle] {
+		signalStrength := *X * *cycle
+		fmt.Printf(msg, *cycle, signalStrength)
+		println()
+		*sum += signalStrength
+	}
+
 }
