03f3a3475948788b51280a9d1d6524795ba67a77
Milan May'r <mayrmilan@gmail.com>
2022-12-20 23:35:11 -0600

    day 10 part two complete!

--- 2022/day-10/main.go
+++ 2022/day-10/main.go
@@ -11,6 +11,8 @@ func main() {
 	sum := sumOfSixSignalStrengths()
 	print("The sum of all six signal strengths is ")
 	println(sum)
+
+	partTwo()
 }
 
 func sumOfSixSignalStrengths() (sum int) {
@@ -51,5 +53,57 @@ func cycleTick(cycle *int, thCycles map[int]bool, X *int, sum *int) {
 		println()
 		*sum += signalStrength
 	}
+}
 
+func partTwo() {
+	input := utils.GetInput("input")
+
+	// create screen
+	screen := make([][]rune, 6)
+	for line := 0; line < 6; line++ {
+		for i := 0; i <= 39; i++ {
+			screen[line] = append(screen[line], '.')
+		}
+	}
+
+	// handle cycles
+	cycle := 0
+	X := 1
+	spritePosition := []int{0, 1, 2}
+
+	for _, line := range input {
+		if line == "noop" {
+			cycleTickAndDrawScreen(&cycle, &X, &screen, &spritePosition)
+		} else if line != "noop" {
+			numberToAdd, _ := strconv.Atoi(line[5:])
+			cycleTickAndDrawScreen(&cycle, &X, &screen, &spritePosition)
+			cycleTickAndDrawScreen(&cycle, &X, &screen, &spritePosition)
+			X += numberToAdd
+			spritePosition = []int{X - 1, X, X + 1}
+		}
+	}
+	// print screen
+	for _, line := range screen {
+		for _, pixel := range line {
+			print(string(pixel))
+		}
+		println()
+	}
+}
+
+func cycleTickAndDrawScreen(cycle *int, X *int, screen *[][]rune, spritePosition *[]int) {
+	*cycle++
+
+	row := *cycle / 40
+	pixel := *cycle%40 - 1
+	if *cycle % 40 == 0 {
+		row = *cycle / 40 -1
+		pixel = 39
+	}
+	// draw screen
+	for _, position := range *spritePosition {
+		if position == pixel {
+			(*screen)[row][pixel] = '#'
+		}
+	}
 }
